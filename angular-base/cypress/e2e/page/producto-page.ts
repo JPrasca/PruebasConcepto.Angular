4527119534f95a119a282bf9f34d56f7beef5c33
Jesus David Prasca Bustos <jdprasca@misena.edu.co>
2022-10-20 14:44:36 -0500

    Implements cy tests

--- angular-base/cypress/e2e/page/producto-page.ts
+++ angular-base/cypress/e2e/page/producto-page.ts
@@ -1,6 +1,6 @@
 export const page = {
   irFormularioCrear: () => {
-    cy.visit('/');
+    cy.visit('http://localhost:4200/');
     cy.get('app-navbar > nav > a:nth-child(2)').click();
     cy.get('#linkCrearProducto').click();
   },
